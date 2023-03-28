eb88899c316a3ebf08f91fba6067106d10625c7f
Ross van Zyl <rossvz@gmail.com>
2023-03-28 16:32:33 -0400

    add copilot and catppucchin colortheme

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
     },
   },
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "catppuccin",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
@@ -29,21 +29,15 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true,     -- enable or disable format on save globally
-        allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
+        enabled = true,      -- enable or disable format on save globally
+        allow_filetypes = {  -- enable format on save for specified filetypes only
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
-          -- "python",
         },
       },
-      disabled = { -- disable formatting capabilities for the listed language servers
-        -- "sumneko_lua",
+      disabled = {       -- disable formatting capabilities for the listed language servers
       },
       timeout_ms = 1000, -- default format timeout
-      -- filter = function(client) -- fully override the default formatting function
-      --   return true
-      -- end
     },
     -- enable servers that you already have installed without mason
     servers = {
