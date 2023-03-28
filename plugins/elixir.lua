acd0f9c3e70483a39336df93d537725847b33fb7
Ross van Zyl <rossvz@gmail.com>
2023-03-28 15:03:10 -0400

    elixir plugin (disabled)

---
+++ plugins/elixir.lua
@@ -0,0 +1,45 @@
+return {
+  -- "mhanberg/elixir.nvim",
+  -- ft = { "elixir", "eex", "heex", "surface" },
+  -- config = function()
+  --   local elixir = require("elixir")
+  --
+  --   elixir.setup {
+  --     settings = elixir.settings {
+  --       dialyzerEnabled = false,
+  --       enableTestLenses = true,
+  --     },
+  --     log_level = vim.lsp.protocol.MessageType.Log,
+  --     message_level = vim.lsp.protocol.MessageType.Log,
+  --     on_attach = function(client, bufnr)
+  --       local map_opts = { buffer = true, noremap = true }
+  --
+  --       -- run the codelens under the cursor
+  --       vim.keymap.set("n", "<space>r", vim.lsp.codelens.run, map_opts)
+  --       -- remove the pipe operator
+  --       vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", map_opts)
+  --       -- add the pipe operator
+  --       vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", map_opts)
+  --       vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", map_opts)
+  --
+  --       -- bindings for standard LSP functions.
+  --       vim.keymap.set("n", "<space>df", "<cmd>lua vim.lsp.buf.format()<cr>", map_opts)
+  --       vim.keymap.set("n", "<space>gd", "<cmd>lua vim.diagnostic.open_float()<cr>", map_opts)
+  --       vim.keymap.set("n", "<space>dt", "<cmd>lua vim.lsp.buf.definition()<cr>", map_opts)
+  --       vim.keymap.set("n", "<space>K", "<cmd>lua vim.lsp.buf.hover()<cr>", map_opts)
+  --       vim.keymap.set("n", "<space>gD", "<cmd>lua vim.lsp.buf.implementation()<cr>", map_opts)
+  --       vim.keymap.set("n", "<space>1gD", "<cmd>lua vim.lsp.buf.type_definition()<cr>", map_opts)
+  --       -- keybinds for fzf-lsp.nvim: https://github.com/gfanto/fzf-lsp.nvim
+  --       -- you could also use telescope.nvim: https://github.com/nvim-telescope/telescope.nvim
+  --       -- there are also core vim.lsp functions that put the same data in the loclist
+  --       vim.keymap.set("n", "<space>gr", ":References<cr>", map_opts)
+  --       vim.keymap.set("n", "<space>g0", ":DocumentSymbols<cr>", map_opts)
+  --       vim.keymap.set("n", "<space>gW", ":WorkspaceSymbols<cr>", map_opts)
+  --       vim.keymap.set("n", "<leader>d", ":Diagnostics<cr>", map_opts)
+  --     end
+  --   }
+  -- end,
+  -- dependencies = {
+  --   "nvim-lua/plenary.nvim",
+  -- }
+}
